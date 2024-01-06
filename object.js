8ceaa38121767768cfa4393b38fa076359549f99
swadebsharma6 <swadebriva2012@gmail.com>
2024-01-06 17:18:15 +0600

    final push

---
+++ object.js
@@ -0,0 +1,11 @@
+const person ={
+    name: 'Swadeb',
+    age: 28,
+    job: 'Developer'
+}
+
+person.salary = 10000;
+
+console.log(Object.keys(person))
+console.log(Object.values(person))
+console.log(Object.entries(person))
