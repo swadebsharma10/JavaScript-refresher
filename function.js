4a01270d2b469c8909bc0370b8963af6d9af1676
swadebsharma6 <swadebriva2012@gmail.com>
2024-01-06 13:46:37 +0600

    refresh javascript

---
+++ function.js
@@ -0,0 +1,28 @@
+// regular function 
+function hello1(){
+    console.log("Hello World");
+    // return undefined;
+}
+let msg = hello1();
+console.log(msg)
+
+
+// function Expression
+const hello2 = function(){
+    console.log("Hello World")
+}
+
+hello2();
+
+// named function Expression
+const hello3 = function hello(){
+    console.log('Hello World Named')
+}
+hello3()
+
+
+// arrow Function
+const hello4 =()=>{
+    console.log("Hello World Arrow function")
+}
+hello4();
---
+++ index.js
@@ -0,0 +1,18 @@
+var lang ="Bangka"
+
+function learn(topic){
+  var  lang = topic;
+
+  if(true){
+    const a = 10;
+    //  a = 6;
+    console.log(a)
+  }
+//   console.log(a) we can not access it
+
+    console.log(`I am learning ${topic}`)
+}
+
+learn("Javascript")
+
+console.log(`I Know ${lang}`)
